55f50d5825afddc212c839fc53e3dea318801662
Sergey Lobin <sergey.lobin@gmail.com>
2019-03-09 18:59:35 +0300

    use in-memory sqlite db

---
+++ tests/Feature/TicketsTests.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+
+class TicketsTests extends TestCase
+{
+
+    public function setUp() : void
+    {
+        parent::setUp();
+        \Artisan::call('migrate');
+        \Artisan::call('db:seed');
+    }
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testTickets()
+    {
+
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get('/tickets');
+
+        $response->assertStatus(200);
+        $response->assertViewIs('user.index');
+    }
+}
+
